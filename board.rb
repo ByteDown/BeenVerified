be42dd85daafc9e494f263116c973be3379b2e29
bytedown <igor.yuzovitskiy@gmail.com>
2016-03-02 13:33:39 -0500

    Add terminal representation of board state.

---
+++ board.rb
@@ -0,0 +1,20 @@
+class Board
+  attr_reader :spaces, :columns
+  
+  def initialize
+    @spaces = Array.new(9)
+    @columns = 3
+  end
+  
+  def display_board
+    output = ""
+    spaces.each_index do |position|
+      output << " #{spaces[position] || position} "
+      case position % columns
+      when 0, 1 then output << "|"
+      when 2 then output << "\n------------\n" unless position == 8
+      end
+    end
+    puts output
+  end
+end
