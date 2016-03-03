2ee64e8af21a7acf92805ce45df22dcf7acbb5ea
bytedown <igor.yuzovitskiy@gmail.com>
2016-03-02 22:34:46 -0500

    Add option to select player1 & player2.

--- console.rb
+++ console.rb
@@ -1,34 +1,44 @@
 require_relative 'board'
+require_relative 'human'
+require_relative 'computer'
 
 class Console
   def banner
     puts "
     XXXXXX OO  XXXX    XXXXXX    O    XXXX    XXXXXX OOOOO  XXXX
       XX   OO  XX        XX     OOO   XX        XX   OO OO  XXXX
-      XX   OO  XXXX      XX    OO OO  XXXX      XX   OOOOO  XXXX
-      
-    Press Enter To Play"
-    enter = gets.chomp
-    select_player if enter
+      XX   OO  XXXX      XX    OO OO  XXXX      XX   OOOOO  XXXX  
+    "
+    select_player
   end
   
   def select_player
     puts "
-    Select first player: (c)omputer or (h)uman"
+    Select first player and press enter: (c)omputer or (h)uman"
     player_one = gets.chomp
+    if "c" === player_one
+      Computer.new
+    else
+      Human.new
+    end
     puts "
-    Select second player: (c)omputer or (h)uman"
+    Select second player and press enter: (c)omputer or (h)uman"
     player_two = gets.chomp
+    if "c" === player_two
+      Computer.new
+    else
+      Human.new
+    end
     randomize_turn
   end
   
   def randomize_turn
-    "Randomizing turn..."
+    puts "Randomizing turn..."
     display_rules
   end
   
   def display_rules
-    "Enter position number to place X or O on the board."
+    puts "Enter position number to place X or O on the board."
     display_board
   end
   
---
+++ human.rb
@@ -0,0 +1,4 @@
+class Human
+  def initialize
+  end
+end
