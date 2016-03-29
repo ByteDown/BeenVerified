3f02b5bef9022cf782c86c905c147eeff3d5cddb
bytedown <igor.yuzovitskiy@gmail.com>
2016-03-29 15:49:48 -0400

    Abstract init methods.

--- init.rb
+++ init.rb
@@ -3,11 +3,7 @@ require_relative 'game'
 class Init
   def initialize
     new_game = Game.new
-    new_game.select_players
-    new_game.activate_first_player
-    new_game.set_marks
-    new_game.rules
-    new_game.process_moves
+    new_game.begin
   end
 end
 
